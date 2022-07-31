c5661356860bd011a8c067648bdcad3ec8f4fc0c
iamagf <agf0710@gmail.com>
2022-07-31 12:23:00 +0900

    Sponsor

--- front/src/Sponsor.jsx
+++ front/src/Sponsor.jsx
@@ -1,7 +1,48 @@
-function Sponsor(data){
-    return <>
-        <h3>If an user of the community decides to support a collector's fundraising, he will sponsor and the assets will be sent to the SC.</h3>
-    </>
+import { useState, useEffect } from 'react';
+
+const axios = require('axios').default;
+const path = "http://127.0.0.1:5000/";
+
+function Sponsor(data) {
+
+
+    const [content, setContent] = useState(
+        ""
+    );
+    const [message, setMessage] = useState('');
+
+    const handleChange = event => {
+        setMessage(event.target.value);
+    }
+
+
+    async function runContent() {
+        axios.get(path + 'sponsor/')
+            .then(function (response) {
+                let content = response.data;
+                setContent(content);
+            })
+        return null;
+    }
+
+    return (
+        <div className='sponsor'>
+            <h3>If an user of the community decides to support a collector's fundraising, he will sponsor and the assets will be sent to the SC, giving a .</h3>
+
+            <label><p>Enter the smart contract address:</p>
+                <br />
+                <input type="text" className='textoGrande'
+                    id="message"
+                    onChange={handleChange}
+                    value={message} />
+                <div className='buttonInput'
+
+                    onClick={runContent()}>
+                    Click here!
+                </div>
+                {content}
+            </label>
+        </div>);
 }
 
 export default Sponsor;
