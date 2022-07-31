ec17fb577ef3c7f796717a4f5187433ab03346fb
iamagf <agf0710@gmail.com>
2022-07-31 11:56:04 +0900

    Corrected to Object structure for calls

--- front/src/Accounts.jsx
+++ front/src/Accounts.jsx
@@ -7,18 +7,18 @@ const path = "http://127.0.0.1:5000/";
 
 function Accounts(data) {
     const [content, setContent] = useState(
-        [
-            {
+        {
+            0: {
                 "addr": "wasm1p7uzckyck0r7e7feqzry6yzmtelq0y95erey3qnjjlyw7q2ludas0850dc",
                 "name": "icaissuer",
                 "creator": "wasm1p7uzckyck0r7e7feqzry6yzmtelq0y95erey3qnjjlyw7q2ludas0850dc"
             },
-            {
+            1: {
                 "addr": "wasm1qnk2n4nlkpw9xfqntladh74w6ujtulwn5wkh5w",
                 "name": "empower",
                 "creator": "wasm1qnk2n4nlkpw9xfqntladh74w6ujtulwn5wkh5w"
             }
-        ]
+        }
     );
 
     useEffect(() => {
@@ -46,7 +46,8 @@ function Accounts(data) {
                 </tr>
             </thead>
             <tbody>
-                {content.map((account) => {
+                {Object.keys(content).map((container, item) => {
+                    let account = content[container];
                     console.log(content);
                     const address = account.addr;
                     const reducedAddress = address.substring(0, 5) + "..." + address.substring(address.length - 6, address.length - 1);
